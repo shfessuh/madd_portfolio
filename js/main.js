38f2878fa95320eaebef16a1f0ee5aed05d267fc
shfessuh <136008610+shfessuh@users.noreply.github.com>
2025-05-02 15:41:55 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -262,7 +262,7 @@ if (
   function createMonitorField() {
     monitors.forEach(m=>scene.remove(m)); monitors.length=0;
     vidTextures.forEach((tex,i)=>{
-      const m = monitorPrototype.clone();
+      const m = monitorPrototype.clone(); m.visible=false;
       m.traverse(n=>{
         if(!n.isMesh||n.name!=='Node-Mesh_2') return;
         const g=n.geometry;
